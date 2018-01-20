d5286d74baaa5a82a2ff124bad9c2779fdd972f7
Rajesh Bhat <Rajesh.Bhat@walmart.com>
2018-01-20 23:26:25 +0530

    automation script

---
+++ for_all_cantos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+chmod a+x ./tesseract_ocr.sh
+./tesseract_ocr.sh ./canto1 lat 2
+./tesseract_ocr.sh ./canto2 lat 2
+./tesseract_ocr.sh ./canto3 lat 2
+./tesseract_ocr.sh ./canto4 lat 2
+./tesseract_ocr.sh ./canto5 lat 2
+./tesseract_ocr.sh ./canto6 lat 2
+./tesseract_ocr.sh ./canto7 lat 2
+./tesseract_ocr.sh ./canto8 lat 2
+./tesseract_ocr.sh ./canto9 lat 2
+./tesseract_ocr.sh ./canto10 lat 2
+./tesseract_ocr.sh ./canto11 lat 2
+./tesseract_ocr.sh ./canto12 lat 2
